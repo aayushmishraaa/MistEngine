04213b0418b6ccd10102388b6adbf6140419c366
Aayush Mishra <78012607+aayushmishraaa@users.noreply.github.com>
2025-06-02 02:34:42 +0100

    Revert "Physics Engine implementaion"
    This reverts commit 7977a99e629140d7509f955e4991e10d30301c47.

--- src/RigidBody.cpp
+++
@@ -1,5 +0,0 @@
-c++
-#include "RigidBody.h"
-
-// No implementation needed in the .cpp file for now, as the update function is in the header (for simplicity of this example).
-// If the update function becomes more complex, it should be moved here.
